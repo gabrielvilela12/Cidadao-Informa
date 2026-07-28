f3813d5613c0e1000e8d3762c022531d2db2ef71
Gabriel Vilela Peixoto <gabrielvipeixoto7@gmail.com>
2026-07-28 10:21:28 -0300

    Open Vercel container port before Spring bootstrap

---
+++ backend-java/start-vercel.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+public_port="${PORT:-80}"
+internal_port="${APP_INTERNAL_PORT:-8081}"
+
+socat \
+  "TCP-LISTEN:${public_port},fork,reuseaddr" \
+  "TCP:127.0.0.1:${internal_port},retry=60,interval=1" &
+
+export PORT="${internal_port}"
+
+exec /opt/java/openjdk/bin/java \
+  -XX:MaxRAMPercentage=75.0 \
+  -jar /app/app.jar
